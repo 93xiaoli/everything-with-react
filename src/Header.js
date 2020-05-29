dee17c7407b61875068329e27b2b6fa9b68e7759
93xiaoli <93xiaoli@gmail.com>
2020-05-28 21:11:41 -0700

    add tab

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function Header() {
+    const [tabs, setTabs] = useState([])
+    const [curValue, setCurValue] = useState('')
+    function handleClick() {
+        if (curValue !== "") {
+            setTabs(tabs.concat(curValue));
+            setCurValue("");
+        }
+    }
+
+    function deleteTab(ind) {
+        let newArr = [...tabs];
+        newArr.splice(ind, 1)
+        setTabs(newArr);
+    }
   return (
     <div>
-        a
+        {tabs.map((tab, index) => (
+            <button key={index}>{tab}</button>
+        ))}
+        <p>Step 1: Add New Tabs</p>
+        <input onChange={event => setCurValue(event.target.value)} value={curValue} />
+        <button onClick={handleClick}>Add</button>
+        <p>Step 2: Delete Tab You Don't Want</p>
+        <p>Tabs List</p>
+        {tabs.map((tab, index) => (
+            <div key={index}>
+                <p>{index + 1}: {tab}</p>
+                <button onClick={() => deleteTab(index)}>Delete</button>
+            </div>
+        ))}
     </div>
   );
 }
